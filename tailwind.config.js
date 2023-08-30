8f468b905c406a77d30fb0d2d05e4176d9e0e74e
sarahdewitt <sarahdewitt15@gmail.com>
2023-08-30 15:34:27 +0800

    install tailwind and done preflight

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      'c-yellow': '#D2EC31',
+      'c-blue': '#1E26DD',
+      'c-orange': '#FF4121',
+      'c-white': '#EDF2F4',
+      'c-black': '#040F0F'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
